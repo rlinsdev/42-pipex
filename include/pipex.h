5071ddac4f65d542ebd74943ec242c919124f756
Rodrigo Lins <rlinsdev@gmail.com>
2022-08-31 06:54:03 -0300

    Test 4

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 07:17:02 by rlins             #+#    #+#             */
-/*   Updated: 2022/08/30 07:53:16 by rlins            ###   ########.fr       */
+/*   Updated: 2022/08/30 21:53:58 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,6 @@ int	start(int argc, char **argv);
 int	startTest(int argc, char **argv);
 int	startTest2(int argc, char **argv);
 int	startTest3(int argc, char **argv);
+int	startTest4(int argc, char **argv);
 
 #endif
--- src/test4.c
+++ src/test4.c
@@ -1,23 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   app.c                                              :+:      :+:    :+:   */
+/*   test4.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rlins <rlins@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/04 18:55:06 by rlins             #+#    #+#             */
-/*   Updated: 2022/08/30 21:52:11 by rlins            ###   ########.fr       */
+/*   Created: 2022/08/29 07:44:13 by rlins             #+#    #+#             */
+/*   Updated: 2022/08/31 06:50:29 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 #include "../include/pipex.h"
 
-int	main(int argc, char **argv)
+int	startTest4(int argc, char **argv)
 {
-	// start(argc, argv);
-	// startTest(argc, argv);
-	// startTest2(argc, argv);
-	// startTest3(argc, argv);
-	startTest4(argc, argv);
+	int	id = fork();
+	if (id == 0)
+	{
+		sleep(1);
+	}
+	printf("Current Id: %d, ParentId: %d\n", getpid(), getppid());
+
+	int res = wait(NULL);
+	if (res == -1)
+		printf("No children to wait for\n");
+	else
+		printf("%d finished execution\n", res);
 	return (0);
 }
