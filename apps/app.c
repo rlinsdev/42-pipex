ffe5b0bf3e20742a6388d671ae26c8713f83b29a
Rodrigo Lins <rlinsdev@gmail.com>
2022-09-07 11:54:16 -0300

    verify argument variables result

--- apps/app.c
+++ apps/app.c
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/04 18:55:06 by rlins             #+#    #+#             */
-/*   Updated: 2022/09/07 06:28:41 by rlins            ###   ########.fr       */
+/*   Updated: 2022/09/07 11:52:22 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,24 @@
  * @param argc Arguments Count
  * @param argv Arguments Vector
  * @param envp Contains all possible paths where the system might store the
- * program
+ * program. All the sys Variables
  * @return int
  */
 int	main(int argc, char **argv, char **envp)
 {
-	// int i = 0;
-	// int j = 0;
-	// while (envp[i][j] != '\0')
-	// {
-	// 	ft_printf("%s\n", envp[i][j]);
-	// 	j++;
-	// }
+	// Code to check ArgVParams
+	for (int j = 0; j < argc; j++)
+		ft_printf("argv[%d]: %s\n", j, argv[j]);
+	ft_printf("\n\n");
+
+	// Code to check all the envp variables.
+	int j = 0;
+	while (envp[j]!= NULL)
+	{
+		ft_printf("envp[%d]: %s\n", j, envp[j]);
+		j++;
+	}
+
 	start(argc, argv, envp);
 	return (0);
 }
