704b1465818623ac10c239d01e1e46a45c55cbd0
Rodrigo Lins <rlinsdev@gmail.com>
2022-09-08 20:37:47 -0300

    Getting better with error handler

--- src/error_handler.c
+++ src/error_handler.c
@@ -6,42 +6,40 @@
 /*   By: rlins <rlins@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/04 16:15:39 by rlins             #+#    #+#             */
-/*   Updated: 2022/09/07 20:22:21 by rlins            ###   ########.fr       */
+/*   Updated: 2022/09/08 19:48:29 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <pipex.h>
 
-void	error_data_handler (t_data data)
+void	error_pipe_handler(t_data data)
 {
-	if (data.pipe_result == -1)
+	if (data.pipe_status == -1)
 	{
-		ft_printf("%s", E_PIPE_MSG);
-		exit(1);
-	}
-	if (data.fd_in == -1)
-	{
-		ft_printf("%s", ACCESS_DEN);
-		exit(1);
-	}
-	if (data.fd_out == -1)
-	{
-		ft_printf("%s", E_INV_PARA);
-		exit(1);
-	}
-	if (data.pid == -1)
-	{
-		ft_printf("%s", "Was not possible to generate fork");
+		perror(E_PIPE_MSG);
 		exit(1);
 	}
 }
 
-void	error_handler(int code)
+void	error_fd_handler (t_data data)
 {
-	// TODO: Chamar PrintF, nao write
+	if (data.fd_in == -1)
+	{
+		perror(ACCESS_DEN);
+		exit(1);
+	}
+	if (data.fd_out == -1)
+	{
+		perror(E_OPEN_OUT);
+		exit(1);
+	}
+}
+
+void	error_args_handler(int code)
+{
 	if (code == ERROR_ARGS)
 	{
-		write(STDERR, E_ARGS_MSG, ft_strlen(E_ARGS_MSG));
-		exit(code);
+		perror(E_ARGS_MSG);
+		exit(1);
 	}
 }
