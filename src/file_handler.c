1b67034d6a2e981ddae5d4af60c86ac130831c42
Rodrigo Lins <rlinsdev@gmail.com>
2022-09-08 23:08:50 -0300

    Open file

--- src/file_handler.c
+++ src/file_handler.c
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 07:22:53 by rlins             #+#    #+#             */
-/*   Updated: 2022/09/07 18:47:12 by rlins            ###   ########.fr       */
+/*   Updated: 2022/09/08 20:11:18 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,12 @@ int	file_open(char *file, int mode)
 {
 	if (mode == IN)
 	{
-		// if (access(filename, F_OK))
-		// {
-		// 	perror(" ");
-		// 	exit(ERROR);
-		// }
 		return (open(file, O_RDONLY, 0644));
 	}
 	else
 	{
+		// TODO: Remove - To test forcing error:
+		// file = "test_test_";
 		/* Flags. Itś possible add multiple flags.
 		 * (Truncated to 0 if something exist in file),
 		 * (if file does not exist, create)
