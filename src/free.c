f72d7185454a591abe262d974a50eeabe96d318c
Rodrigo Lins <rlinsdev@gmail.com>
2022-09-08 23:08:31 -0300

    Free memory

--- src/free.c
+++ src/free.c
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 20:29:57 by rlins             #+#    #+#             */
-/*   Updated: 2022/09/08 20:33:13 by rlins            ###   ########.fr       */
+/*   Updated: 2022/09/08 22:47:50 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,4 +39,10 @@ void	child_process_free(t_data *data)
 	}
 	free(data->cmd_args);
 	free(data->cmd);
+}
+
+void	close_pipes_fd(t_data *data)
+{
+	close(data->pipe_fd[0]);
+	close(data->pipe_fd[1]);
 }
