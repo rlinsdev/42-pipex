dca4f8688af3ffb04b5655f7ec7aec8ed9aed163
Rodrigo Lins <rlinsdev@gmail.com>
2022-09-12 08:41:33 -0300

    Fixing execute class

--- src/execute_cmd.c
+++ src/execute_cmd.c
@@ -6,44 +6,41 @@
 /*   By: rlins <rlins@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 19:59:00 by rlins             #+#    #+#             */
-/*   Updated: 2022/09/08 22:43:41 by rlins            ###   ########.fr       */
+/*   Updated: 2022/09/12 08:40:32 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include <pipex.h>
 
-static char	*get_cmd (t_data data)
+static char	*get_cmd (char **paths, char *cmd)
 {
-	char	*aux;
+	char	*tmp;
 	char	*command;
-	// data.cmd_path
-	// while (*paths)
-	while (*data.cmd_path)
+
+	while (*paths)
 	{
-		// aux = ft_strjoin(*paths, "/");
-		aux = ft_strjoin(*data.cmd_path, "/");
-		command = ft_strjoin(aux, data.cmd);
-		free(aux);
+		tmp = ft_strjoin(*paths, "/");
+		command = ft_strjoin(tmp, cmd);
+		free(tmp);
 		if (access(command, 0) == 0)
 			return (command);
 		free(command);
-		// paths++;
-		*data.cmd_path++;
+		paths++;
 	}
 	return (NULL);
 }
 
-static void	call_run(t_data data)
-{
-	data.cmd = get_cmd(data);
-	if (!data.cmd)
-	{
-		child_process_free(&data);
-		perror(INV_CMD);
-		exit(1);
-	}
-	execve(data.cmd, data.cmd_args, data.envp);
-}
+// static void	call_run(t_data data)
+// {
+// 	data.cmd = get_cmd(data);
+// 	if (!data.cmd)
+// 	{
+// 		child_process_free(&data);
+// 		perror(INV_CMD);
+// 		exit(1);
+// 	}
+// 	execve(data.cmd, data.cmd_args, data.envp);
+// }
 
 void	first_child(t_data data)
 {
@@ -51,14 +48,30 @@ void	first_child(t_data data)
 	close(data.pipe_fd[0]);
 	dup2(data.fd_in, 0);
 	data.cmd_args = ft_split(data.argv[2], ' ');
-	call_run(data);
+	//call_run(data);
+	data.cmd = get_cmd(data.cmd_path, data.cmd_args[0]);
+	if (!data.cmd)
+	{
+		child_process_free(&data);
+		// msg(INV_CMD);
+		exit(1);
+	}
+	execve(data.cmd, data.cmd_args, data.envp);
 }
 
 void	second_child(t_data data)
 {
 	dup2(data.pipe_fd[0], 0);
 	close(data.pipe_fd[1]);
-	dup2(data.fd_in, 1);
+	dup2(data.fd_out, 1);
 	data.cmd_args = ft_split(data.argv[3], ' ');
-	call_run(data);
+	//call_run(data);
+	data.cmd = get_cmd(data.cmd_path, data.cmd_args[0]);
+	if (!data.cmd)
+	{
+		child_process_free(&data);
+		// msg(INV_CMD);
+		exit(1);
+	}
+	execve(data.cmd, data.cmd_args, data.envp);
 }
