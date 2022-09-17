be624a464e5f6ab66a507727ea695e195761487f
Rodrigo Lins <rlinsdev@gmail.com>
2022-09-17 13:04:24 -0300

    app.c comments

--- apps/app.c
+++ apps/app.c
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/04 18:55:06 by rlins             #+#    #+#             */
-/*   Updated: 2022/09/14 07:37:28 by rlins            ###   ########.fr       */
+/*   Updated: 2022/09/17 13:03:46 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,23 +22,18 @@
  */
 int	main(int argc, char *argv[], char *envp[])
 {
-	// Code to check ArgVParams
-	for (int j = 0; j < argc; j++)
-		ft_printf("argv[%d]: %s\n", j, argv[j]);
-	ft_printf("\n\n");
+	// // Code to check ArgvParams
+	// for (int j = 0; j < argc; j++)
+	// 	ft_printf("argv[%d]: %s\n", j, argv[j]);
+	// ft_printf("\n\n");
 
-	// Code to check all the envp variables.
+	// // Code to check all the envp variables.
 	// int j = 0;
 	// while (envp[j]!= NULL)
 	// {
 	// 	ft_printf("envp[%d]: %s\n", j, envp[j]);
 	// 	j++;
 	// }
-
-	// ft_printf(ft_split((char **)"ls -l", ' '));
-	// ft_printf(ft_split((char **)"wc -l", ' '));
-
-
 
 	start(argc, argv, envp);
 	return (0);
