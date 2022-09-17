b32cdb5601d5a8935e14aefb79f2ee4708f9e19d
Rodrigo Lins <rlinsdev@gmail.com>
2022-09-17 13:32:12 -0300

    comments in Pipex.c

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 07:17:21 by rlins             #+#    #+#             */
-/*   Updated: 2022/09/14 10:19:03 by rlins            ###   ########.fr       */
+/*   Updated: 2022/09/17 13:16:14 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,14 @@
 
 /**
  * @brief Will loop through all variables to retrieve the value of PATH variable
+ * In all the EnvVariables (more than 70) I just need the PATH
  * @param envp variables of environment
  * @return char* result of PATH variable
  */
 static char	*path_handler(char **envp)
 {
-	// Find PATH= in envp variable
+	/* PATH  shows all possible paths towards program binary files,
+	 * including shell commands */
 	while (ft_strncmp("PATH=", *envp, 5))
 		envp++;
 	return (*envp + 5); // Remove 'PATH=' and return all content
@@ -27,7 +29,7 @@ static char	*path_handler(char **envp)
 
 /**
  * @brief Initialize all variables in data type
- * @param data Object initialized *
+ * @param data Object initialized
  * @param argc Initialize arg Count
  * @param argv Initialize arg Vector
  * @param envp Initialize Environment variables
@@ -39,37 +41,51 @@ static void	ini_data(t_data *data, int argc, char **argv, char **envp)
 	data->argc = argc;
 	data->argv = argv;
 	data->envp = envp;
+	// Just return the Path info (all string)
 	data->path = path_handler(envp);
+	//ft_printf("\nPath: %s\n", data->path);
+	// Get all in Path variable and just split by ':'
+	data->cmd_path = ft_split(data->path, ':');
+	// int j = 0;
+	// while (data->cmd_path[j]!= NULL)
+	// {
+	// 	ft_printf("cmd_path[%d]: %s\n", j, data->cmd_path[j]);
+	// 	j++;
+	// }
 }
 
 int	start(int argc, char **argv, char **envp)
 {
 	t_data data;
 	ini_data(&data, argc, argv, envp);
-
+	// Must to be 5 elements in count
 	if (argc != 5)
 		error_args_handler(ERROR_ARGS);
-	// Open files
+	// Open file file-in
 	data.fd_in = file_open(argv[1], IN);
-	data.fd_out = file_open(argv[data.argc - 1], OUT); // TODO: data.argc?
+	// Open file file-out
+	data.fd_out = file_open(argv[4], OUT);
 	error_fd_handler(data);
-	//
+	/* Pipe will store 2 FD. Bytes write in [1] can be read from [0]
+	 * Pipe = communication between process */
 	data.pipe_status = pipe(data.pipe_fd);
 	error_pipe_handler(data);
-	data.cmd_path = ft_split(data.path, ':');
-	// Create 2 process
+	/* Create 2 process. Here we must to Fork a process, because after execve
+	 * the program stop, but we don't wanna loose our own process */
 	data.pid1 = fork();
 	// When in child process
 	if (data.pid1 == 0)
 		first_child(data);
-	// Create 2 process
+	/* Create 2 process. Here we must to Fork a process, because after execve
+	 * the program stop, but we don't wanna loose our own process */
 	data.pid2 = fork();
 	// When in child process
 	if (data.pid2 == 0)
-		second_child(data); //TODO: Porque eu criei 2 childs?
+		second_child(data);
+	error_fork_handler(data);
 	close_pipes_fd(&data);
-	// Wait the execution of child process
-	waitpid(data.pid1, NULL, 0);
+	// Wait the execution of child process. This will avoid zombie process
+	waitpid(data.pid1, NULL, 0); // supervising the children
 	waitpid(data.pid2, NULL, 0);
 	main_process_free(&data);
 	return (0);
