2ecfc0c109b58681b0db55dcc42f1b2dd6564fd4
Rodrigo Lins <rlinsdev@gmail.com>
2022-09-17 13:25:01 -0300

    comments to open a file

--- src/file_handler.c
+++ src/file_handler.c
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 07:22:53 by rlins             #+#    #+#             */
-/*   Updated: 2022/09/13 21:12:54 by rlins            ###   ########.fr       */
+/*   Updated: 2022/09/17 13:24:30 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ int	file_open(char *file, int mode)
 	{
 		if (access(file, F_OK))
 			error_open_file();
-		// chmod: ----rw-r--
+		/*  chmod: -rw-r--r--
+		 * Octal number to decimal 420 (0 denote to octal) */
 		return (open(file, O_RDONLY, 0644));
 	}
 	else
